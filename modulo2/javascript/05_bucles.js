0b0325514419e9452839ff8b79a51e7237602db9
hyxuu <hyxuu05@gmail.com>
2025-10-21 10:02:39 -0500

    Clase 2025-10-21

---
+++ modulo2/javascript/05_bucles.js
@@ -0,0 +1,18 @@
+for(let i=1; i <=10;i++){
+    console.log("numero", i);
+}
+
+suma=0;
+for (let i=1; i <=10;i++){
+    suma+=i;
+}
+console.log("sumatoria indices", suma);
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`4 * ${i} = ${4 * i}`);
+}
+
+let nombre="Francisco";
+for(let i=0; i <nombre.length;i++){
+    console.log(nombre[i]);
+}
