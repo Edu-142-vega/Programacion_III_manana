1ec38698330739d9f0a23ad1a99f3f2384369ce6
Eduardo Vega <vega.eduardo5051@gmail.com>
2025-10-21 10:02:52 -0500

    clases 2025-10-21

---
+++ modulo2/javascript/05_bucles.js
@@ -0,0 +1,22 @@
+for(let i=1;<=10;i++){
+    suma+=i
+    console.log("sumatoria de indices",suma);
+
+
+}
+
+for(let i=1;<=10;i++){
+    suma+=i
+    console.log("sumatoria de indices",suma);
+
+}
+
+for (let i = 1; i <= 10; i++) {
+  let resultado = 4 * i;
+  console.log(`4 x ${i} = ${resultado}`);
+}
+console.log-("sumatoria de indices", suma );
+nombre= "francisco";
+for (let i = 0; i < nombre.length; i++) {
+    console.log(nombre[i]);
+}
